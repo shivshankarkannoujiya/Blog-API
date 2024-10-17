b1401b094eadedb1e5040cb5708c1a58925b36b5
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-10-17 11:55:40 +0530

    done initial setup

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: "./.env",
+});
+
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log(`ERROR: ${error.message}`);
+            process.exit(1);
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is listening at PORT: ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(`MongoDB connection Failed, ERROR: ${error.message}`);
+    });
